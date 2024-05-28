7e5064415627c91263eba74b84d888b62b7228dc
kimsang <kimsang.mok01@gmail.com>
2024-05-28 10:53:44 +0700

    fix: fix typo and hanlde api error in the composite service

--- microservices/product-composite-service/src/main/java/com/kimsang/microservices/composite/product/services/ProductCompositeIntegration.java
+++ microservices/product-composite-service/src/main/java/com/kimsang/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -1,6 +1,8 @@
 package com.kimsang.microservices.composite.product.services;
 
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kimsang.api.core.product.Product;
@@ -9,14 +11,20 @@ import com.kimsang.api.core.recommendation.Recommendation;
 import com.kimsang.api.core.recommendation.RecommendationService;
 import com.kimsang.api.core.review.Review;
 import com.kimsang.api.core.review.ReviewService;
+import com.kimsang.api.exceptions.InvalidInputException;
+import com.kimsang.api.exceptions.NotFoundException;
+import com.kimsang.util.http.HttpErrorInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -42,30 +50,92 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
 
     this.productServiceUrl = "http://" + productServiceHost + ":" + productServicePort + "/product/";
     this.recommendationServiceUrl = "http://" + recommendationServiceHost + ":" + recommendationServicePort
-        + "/recommendation?productId = ";
-    reviewServiceUrl = " http://" + reviewServiceHost + ":"
+        + "/recommendation?productId=";
+    reviewServiceUrl = "http://" + reviewServiceHost + ":"
         + reviewServicePort + "/review?productId=";
   }
 
   @Override
   public Product getProduct(int productId) {
-    String url = productServiceUrl + productId;
-    return restTemplate.getForObject(url, Product.class);
+    try {
+      String url = productServiceUrl + productId;
+      LOG.debug("Will call getProduct API on URL: {}", url);
+
+      Product product = restTemplate.getForObject(url, Product.class);
+
+      if (product != null) {
+        LOG.debug("Found a product with id: {}", product.getProductId());
+      }
+      return product;
+
+    } catch (HttpClientErrorException ex) {
+      switch (ex.getStatusCode()) {
+        case NOT_FOUND:
+          throw new NotFoundException(getErrorMessage(ex));
+
+        case UNPROCESSABLE_ENTITY:
+          throw new InvalidInputException(getErrorMessage(ex));
+
+        default:
+          LOG.warn("Got an unexpected HTTP error: {}, will rethrow it", ex.getStatusCode());
+          LOG.warn("Error body: {}", ex.getResponseBodyAsString());
+          throw ex;
+      }
+    }
+  }
+
+  private String getErrorMessage(HttpClientErrorException ex) {
+    try {
+      return mapper.readValue(ex.getResponseBodyAsString(), HttpErrorInfo.class).getMessage();
+    } catch (IOException ioException) {
+      return ex.getMessage();
+    }
   }
 
   @Override
   public List<Recommendation> getRecommendations(int productId) {
-    String url = recommendationServiceUrl + productId;
-    return restTemplate.exchange(url, GET, null, new ParameterizedTypeReference<List<Recommendation>>() {
-    }).getBody();
+
+    try {
+      String url = recommendationServiceUrl + productId;
+
+      LOG.debug("Will call getRecommendations API on URL: {}", url);
+      List<Recommendation> recommendations = restTemplate
+          .exchange(url, GET, null, new ParameterizedTypeReference<List<Recommendation>>() {
+          })
+          .getBody();
+
+      if (recommendations != null) {
+        LOG.debug("Found {} recommendations for a product with id: {}", recommendations.size(), productId);
+      }
+      return recommendations;
+
+    } catch (Exception ex) {
+      LOG.warn("Got an exception while requesting recommendations, return zero recommendations: {}", ex.getMessage());
+      return new ArrayList<>();
+    }
   }
 
   @Override
   public List<Review> getReviews(int productId) {
-    String url = reviewServiceUrl + productId;
-    return restTemplate.exchange(url, GET, null, new ParameterizedTypeReference<List<Review>>() {
-    }).getBody();
 
+    try {
+      String url = reviewServiceUrl + productId;
+
+      LOG.debug("Will call getReviews API on URL: {}", url);
+      List<Review> reviews = restTemplate
+          .exchange(url, GET, null, new ParameterizedTypeReference<List<Review>>() {
+          })
+          .getBody();
+
+      if (reviews != null) {
+        LOG.debug("Found {} reviews for a product with id: {}", reviews.size(), productId);
+      }
+      return reviews;
+
+    } catch (Exception ex) {
+      LOG.warn("Got an exception while requesting reviews, return zero reviews: {}", ex.getMessage());
+      return new ArrayList<>();
+    }
   }
 }
 
